57b83804b1411e208ff51ebed72b785d0afc2593
ben.jeong1 <ben.jeong1@kakaocorp.com>
2022-10-26 12:04:13 +0900

    asdf apply

--- Bin/update-asdf.sh
+++ Bin/update-asdf.sh
@@ -2,11 +2,11 @@
 
 asdf plugin update --all
 
-asdf install rust latest
-asdf install golang latest
-asdf install kotlin latest
-asdf install zig latest
-asdf install python latest
+asdf install rust latest && asdf global rust latest
+asdf install golang latest && asdf global golang latest
+asdf install kotlin latest && asdf global kotlin latest
+asdf install zig latest && asdf global zig latest
+asdf install python latest && asdf global python latest
 
-asdf install java $(asdf latest java zulu-17)
+asdf install java $(asdf latest java zulu-17) && asdf global java $(asdf latest java zulu-17)
 asdf install java $(asdf latest java zulu-8)
