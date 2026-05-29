638f4523d7f16c1aa2ce418d72263808a04c2e3b
ben.jeong1 <ben.jeong1@kakaocorp.com>
2026-05-30 02:31:21 +0900

    .

--- Bin/run-tools-updater.sh
+++ Bin/run-tools-updater.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
+source ~/.zshrc
 brew update; brew upgrade --greedy; brew upgrade --cask --greedy; rustup update stable --force; mise upgrade; cargo install-update -a; omz update
